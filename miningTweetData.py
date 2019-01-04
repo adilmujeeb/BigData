1c6daaaa33f09c691519ae91fe2516ff95747374
adilmujeeb <mujeeb.adil@gmail.com>
2019-01-05 01:23:22 +0530

    Update miningTweetData.py

--- miningTweetData.py
+++ miningTweetData.py
@@ -5,10 +5,10 @@ import json
 def process_or_store(tweet):
     print(json.dumps(tweet))
 
-consumer_key = 'YoF6fIoKyNK7xVzSdPvHLIDyf'
-consumer_secret = '8eT0mwgasUhruN7Rxrb5fTPgAYixT5dxf5HSFJXYAHJVo7lpjQ'
-access_token = '27921499-BxFHm42hWK5AEErzjAQCThZS8o2EGp3SlUJVdyJzd'
-access_secret = 'iaF11XorVbcv9sxpCP0Y8ay0txKxGJ2YcxtRlQm0Xl4Ex'
+consumer_key = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXXXXXXXXXXXXX'
+access_token = 'XXXXXXXXXXXXXXXXXXXXXX'
+access_secret = 'XXXXXXXXXXXXXXXXXXXXXXX'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
